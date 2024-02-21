464a08e9fb5e3bec4506d0bff39d4d106756485d
Anany01T <ananytomar01@gmail.com>
2024-02-21 23:27:13 +0530

    the main files of the project

---
+++ Dijkstra/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header.js';
+import Block from './components/Block.js';
+
+const App = () => {
+  return (
+    <>
+      
+      <div className='mainBlock'>
+      <div className='click'>
+      <Block/>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App;
